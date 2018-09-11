34d952af2dc874c23bff15e3d8de1feb2097ed16
thesnoom <10978987+thesnoom@users.noreply.github.com>
2018-09-11 01:10:40 +0100

    getopt_long outline

--- main.c
+++ main.c
@@ -6,10 +6,19 @@
  * - thesnoom 2018
  */
 
+
 #include <Windows.h>
 #include <stdio.h>
 
 #include "getopt.h"
+#include "log.h"
+
+
+void help()
+{
+	//
+}
+
 
 int main(int argc, char **argv)
 {
@@ -21,6 +30,33 @@ int main(int argc, char **argv)
 			"+-----------------------+\n\n");
 
 
+	static struct option long_opts[] =
+	{
+		{"help",		no_argument, 0, 'h'},
+		{"verbose",		no_argument, 0, 'v'},
+		{0, 0, 0, 0}
+	};
+
+	int opt_idx = 0, c = 0, verbosity = 0;
+
+	while((c = getopt_long(argc, argv, "v", long_opts, &opt_idx)) != -1)
+	{
+		switch(c)
+		{
+			case 'h':
+			{
+				help();
+				return 0;
+			}
+
+			case 'v':
+			{
+				verbosity++;
+			}
+		}
+	}
 
 	return 0;
 }
+
+
