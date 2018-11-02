5c2ec7adc3aea01b631826de246a500c165dea60
snoom <10978987+thesnoom@users.noreply.github.com>
2018-11-02 10:30:20 +0000

    Added domain checks
    Built a local test domain ranging from Win vista -> Windows 10, on a Server 2016 PDC.
    
    Does domain checks successfully. Can now plod on and get the domain shit done.

--- main.c
+++ main.c
@@ -30,16 +30,36 @@ int main(int argc, char **argv)
 	// System Information
 	// ------------------------------------------------
 	printf("[+] Listing system information:\n");
+	printf("-------------------------------\n");
+
 	DisplayWinVerInfo();
 	DisplayCoreInfo();
 	// ------------------------------------------------
 	// ------------------------------------------------
 
 
+
+
 	// ------------------------------------------------
-	// Current User Information
+	// Current User Information & Domain Checks
 	// ------------------------------------------------
 	printf("\n[+] Listing current user information:\n");
+	printf("-------------------------------------\n");
+
+	char szDomTest[256] = { 0 }, szDomFQ[256] = { 0 };
+	DWORD dwDomLen = 256;
+	GetComputerNameEx(ComputerNamePhysicalDnsDomain, szDomTest, &dwDomLen);
+
+	dwDomLen = 256;
+	GetComputerNameEx(ComputerNameDnsFullyQualified, szDomFQ, &dwDomLen);
+
+	if(strlen(szDomTest))
+	{
+		printf("- Domain joined PC: %s\n- PC Domain Name: %s\n", szDomTest, szDomFQ);
+		for(size_t i = 0; i < (18 + strlen(szDomFQ)); printf("-"), i++); puts("");
+
+		// SET DOMAIN FLAG
+	}
 	
 	char *szUser = (char *)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, 64);
 	if(!szUser)
@@ -73,31 +93,45 @@ int main(int argc, char **argv)
 	// ------------------------------------------------
 
 
+
+
 	// ------------------------------------------------
 	// Current Process Privileges
 	// ------------------------------------------------
 	printf("\n[+] Listing current process privileges:\n");
+	printf("---------------------------------------\n");
+
 	LoopTokens();
 	// ------------------------------------------------
 	// ------------------------------------------------
+
+
 
 
 	// ------------------------------------------------
 	// Running Processes
 	// ------------------------------------------------
 	printf("\n[+] Listing running processes:\n");
+	printf("------------------------------\n");
+
 	DisplayProcesses();
 	// ------------------------------------------------
 	// ------------------------------------------------
+
+
 
 
 	// ------------------------------------------------
 	// Local Users & Groups
 	// ------------------------------------------------
 	printf("\n[+] Listing local user accounts:\n");
+	printf("--------------------------------\n");
+
 	LocalInformation(_LOCAL_USERS);
 
 	printf("\n[+] Listing local groups:\n");
+	printf("-------------------------\n");
+
 	LocalInformation(_LOCAL_GROUPS);
 	// ------------------------------------------------
 	// ------------------------------------------------
